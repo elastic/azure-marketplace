1eee0131d586dde5366f6f5505fffd6c60c31c83
Martijn Laarman <mpdreamz@gmail.com>
2016-03-15 11:17:37 +0100

    switch back to allowedSizes

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -57,7 +57,7 @@ gulp.task("patch", function(cb) {
           var masterSizeControl = _.find(nodesStep.elements, function (el) { return el.name == "vmSizeMasterNodes"; });
           var dataSizeControl = _.find(nodesStep.elements, function (el) { return el.name == "vmSizeDataNodes"; });
           var clientSizeControl = _.find(nodesStep.elements, function (el) { return el.name == "vmSizeClientNodes"; });
-          var patchVmSizes = function(control) { control.constraints.includedSizes = vmSizes; }
+          var patchVmSizes = function(control) { control.constraints.allowedSizes = vmSizes; }
           patchVmSizes(masterSizeControl);
           patchVmSizes(dataSizeControl);
           patchVmSizes(clientSizeControl);
