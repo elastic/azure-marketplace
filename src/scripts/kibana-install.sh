48dcab602761ce162e9b717f137dbec366843fb2
Mpdreamz <mpdreamz@gmail.com>
2016-12-20 12:52:46 +0100

    5.x support
    This adds supports for Elasticsearch & Kibana 5.x
    
    Elasticsearch installation:
    
    - improved java installation even further by going nuclear on retry routine cleaning after apt get completely before retrying. Was still seeing random checksum failures quite often
    - XPack security is now all native (REST API) based, no longer based on the file realm
    - Because we do upserts we need to ignore 409 on curl commands
    - Added a wait for online method that waits until elasticsearch is started before firing off REST calls. Could be useful for different things in the future too.
    - `es_admin` no longer exists in 5.x we update the builtin `elastic` superuser instead.
    
    Kibana Installation
    
    - Moved to the debian package for installation
    - Debian package takes care of setting up service and file based logging
    - make sure we set server.host to external ip since kibana 5.x no longer binds to 0.0.0.0 by default (rightfully so!)
    
    ARM Template (jumpbox & kibana)
    
    - `sourcePortRange` needs to be `*` in order for 5601 and 22 to be accessible now that we apply NSG correctly
    
    UI
    - mention the different superuser usernames in the label
    
    build/allowedValues.json
    
    - add `npm run deploy-test` which quickly deploys any `test.parameters.json` file (gitignored) to `westeurope` to ease development
    - `npm run deploy-all` now validates using a variable superuser username based on what version we are testing.

--- src/scripts/kibana-install.sh
+++ src/scripts/kibana-install.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 
-# The MIT License (MIT)
-#
-# Portions Copyright (c) 2015 Microsoft Azure
-# Portions Copyright (c) 2015 Elastic, Inc.
-#
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# License: https://github.com/elastic/azure-marketplace/blob/master/LICENSE.txt
 #
 # Trent Swanson (Full Scale 180 Inc)
-# Martijn Laarman (Elastic)
+# Martijn Laarman, Greg Marzouka, Russ Cam (Elastic)
+# Contributors
 #
 
 #########################
@@ -47,7 +26,8 @@ help()
     echo "-h view this help content"
 }
 
-# log() does an echo prefixed with time
+# Custom logging with time so we can easily relate running times, also log to separate file so order is guaranteed.
+# The Script extension output the stdout/err buffer in intervals with duplicates.
 log()
 {
     echo \[$(date +%d%m%Y-%H:%M:%S)\] "$1"
@@ -65,7 +45,7 @@ if service --status-all | grep -Fq 'kibana'; then
 fi
 
 #########################
-# Paramater handling
+# Parameter handling
 #########################
 
 #Script Parameters
@@ -76,7 +56,6 @@ ES_VERSION="2.0.0"
 ELASTICSEARCH_URL="http://10.0.0.4:9200"
 INSTALL_PLUGINS=0
 HOSTMODE="internal"
-
 USER_KIBANA4_SERVER_PWD="changeME"
 
 #Loop through options passed
@@ -125,106 +104,199 @@ log "installing kibana plugins is set to: $INSTALL_PLUGINS"
 log "Kibana will talk to elasticsearch over $ELASTICSEARCH_URL"
 
 #########################
-# Installation
+# Installation steps as functions
 #########################
 
-sudo groupadd -g 999 kibana
-sudo useradd -u 999 -g 999 kibana
+old_add_kibana_os_user()
+{
+  log "[old_add_kibana_os_user] adding kibana user"
+  sudo groupadd -g 999 kibana
+  sudo useradd -u 999 -g 999 kibana
+}
 
-sudo mkdir -p /opt/kibana
+old_download_unzip_kibana()
+{
+  sudo mkdir -p /opt/kibana
+  if dpkg --compare-versions "$KIBANA_VERSION" ">=" "5.0.0"; then
+      DOWNLOAD_URL="https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-linux-x86_64.tar.gz"
+  elif dpkg --compare-versions "$KIBANA_VERSION" ">=" "4.6.0"; then
+      DOWNLOAD_URL="https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x86_64.tar.gz"
+  else
+      DOWNLOAD_URL="https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz"
+  fi
 
-if dpkg --compare-versions "$KIBANA_VERSION" ">=" "4.6.0"; then
-    DOWNLOAD_URL="https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x86_64.tar.gz"
-else
-    DOWNLOAD_URL="https://download.elastic.co/kibana/kibana/kibana-$KIBANA_VERSION-linux-x64.tar.gz"
-fi
+  log "[old_download_unzip_kibana] downloading kibana $KIBANA_VERSION from $DOWNLOAD_URL"
+  curl -o kibana.tar.gz "$DOWNLOAD_URL"
+  tar xvf kibana.tar.gz -C /opt/kibana/ --strip-components=1
+  log "kibana $KIBANA_VERSION downloaded"
 
-log "downloading kibana $KIBANA_VERSION from $DOWNLOAD_URL"
-curl -o kibana.tar.gz "$DOWNLOAD_URL"
-tar xvf kibana.tar.gz -C /opt/kibana/ --strip-components=1
-log "kibana $KIBANA_VERSION downloaded"
+  sudo chown -R kibana: /opt/kibana
 
-sudo chown -R kibana: /opt/kibana
+  mv /opt/kibana/config/kibana.yml /opt/kibana/config/kibana.yml.bak
+}
 
-mv /opt/kibana/config/kibana.yml /opt/kibana/config/kibana.yml.bak
+download_install_deb()
+{
+    log "[download_install_deb] starting download of package"
+    local DOWNLOAD_URL="https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-amd64.deb"
+    curl -o "kibana-$KIBANA_VERSION.deb" "$DOWNLOAD_URL"
+    log "[download_install_deb] installing downloaded package"
+    sudo dpkg -i "kibana-$KIBANA_VERSION.deb"
+}
 
-# set the elasticsearch URL
-echo "elasticsearch.url: \"$ELASTICSEARCH_URL\"" >> /opt/kibana/config/kibana.yml
-# specify kibana log location
-echo "logging.dest: /var/log/kibana.log" >> /opt/kibana/config/kibana.yml
+## Security
+##----------------------------------
 
-if [ ${INSTALL_PLUGINS} -ne 0 ]; then
+old_configuration_and_plugins()
+{
+    log "[old_configuration_and_plugins] configuring kibana.yml"
+    # set the elasticsearch URL
+    echo "elasticsearch.url: \"$ELASTICSEARCH_URL\"" >> /opt/kibana/config/kibana.yml
+    # specify kibana log location
+    echo "logging.dest: /var/log/kibana.log" >> /opt/kibana/config/kibana.yml
     echo "elasticsearch.username: es_kibana_server" >> /opt/kibana/config/kibana.yml
     echo "elasticsearch.password: \"$USER_KIBANA4_SERVER_PWD\"" >> /opt/kibana/config/kibana.yml
 
     # install shield only on Elasticsearch 2.4.0+ so that graph can be used.
-    # cannot be installed on earlier versions as 
+    # cannot be installed on earlier versions as
     # they do not allow unsafe sessions (i.e. sending session cookie over HTTP)
     if dpkg --compare-versions "$ES_VERSION" ">=" "2.4.0"; then
-      log "installing latest shield"
+      log "[old_configuration_and_plugins] installing latest shield"
       /opt/kibana/bin/kibana plugin --install kibana/shield/2.4.0
-      log "shield plugin installed"
+      log "[old_configuration_and_plugins] shield plugin installed"
 
-      # NOTE: These settings allow Shield to work in Kibana without HTTPS. 
+      # NOTE: These settings allow Shield to work in Kibana without HTTPS.
       # This is NOT recommended for production.
       echo "shield.useUnsafeSessions: true" >> /opt/kibana/config/kibana.yml
       echo "shield.skipSslCheck: true" >> /opt/kibana/config/kibana.yml
 
-      log "generating shield encryption key"
-      if [ $(dpkg-query -W -f='${Status}' pwgen 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        (sudo apt-get -yq install pwgen || (sleep 15; sudo apt-get -yq install pwgen))
-      fi
-      ENCRYPTION_KEY=$(pwgen 64 1)    
+      install_pwgen
+
+      log "[old_configuration_and_plugins] generating shield encryption key"
+      ENCRYPTION_KEY=$(pwgen 64 1)
       echo "shield.encryptionKey: \"$ENCRYPTION_KEY\"" >> /opt/kibana/config/kibana.yml
-      log "shield encryption key generated"    
+      log "[old_configuration_and_plugins] shield encryption key generated"
     fi
 
     # install graph
     if dpkg --compare-versions "$ES_VERSION" ">=" "2.3.0"; then
-      log "installing graph plugin"
+      log "[old_configuration_and_plugins] installing graph plugin"
       /opt/kibana/bin/kibana plugin --install elasticsearch/graph/$ES_VERSION
-      log "graph plugin installed"
+      log "[old_configuration_and_plugins] graph plugin installed"
     fi
 
     # install reporting
     if dpkg --compare-versions "$KIBANA_VERSION" ">=" "4.6.1"; then
-      log "installing reporting plugin"
+      log "[old_configuration_and_plugins] installing reporting plugin"
       /opt/kibana/bin/kibana plugin --install kibana/reporting/2.4.1
-      log "reporting plugin installed"
+      log "[old_configuration_and_plugins] reporting plugin installed"
 
-      log "generating reporting encryption key"
+      log "[old_configuration_and_plugins] generating reporting encryption key"
       if [ $(dpkg-query -W -f='${Status}' pwgen 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         (sudo apt-get -yq install pwgen || (sleep 15; sudo apt-get -yq install pwgen))
       fi
-      ENCRYPTION_KEY=$(pwgen 64 1)    
+      ENCRYPTION_KEY=$(pwgen 64 1)
       echo "reporting.encryptionKey: \"$ENCRYPTION_KEY\"" >> /opt/kibana/config/kibana.yml
-      log "reporting encryption key generated"    
+      log "[old_configuration_and_plugins] reporting encryption key generated"
     fi
 
-    log "installing monitoring plugin"
+    log "[old_configuration_and_plugins] installing monitoring plugin"
     /opt/kibana/bin/kibana plugin --install elasticsearch/marvel/$ES_VERSION
-    log "monitoring plugin installed"
-    log "installing sense plugin"
+    log "[old_configuration_and_plugins] monitoring plugin installed"
+    log "[old_configuration_and_plugins] installing sense plugin"
     /opt/kibana/bin/kibana plugin --install elastic/sense
-    log "sense plugin installed"
+    log "[old_configuration_and_plugins] sense plugin installed"
 
     # sense default url to point at Elasticsearch on first load
     echo "sense.defaultServerUrl: \"$ELASTICSEARCH_URL\"" >> /opt/kibana/config/kibana.yml
+}
+
+install_pwgen()
+{
+    log "[install_pwgen] installing pwgen tool if needed"
+    if [ $(dpkg-query -W -f='${Status}' pwgen 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+      (sudo apt-get -yq install pwgen || (sleep 15; sudo apt-get -yq install pwgen))
+    fi
+}
+
+configuration_and_plugins()
+{
+    log "[configuration_and_plugins] configuring kibana.yml"
+    local KIBANA_CONF=/etc/kibana/kibana.yml
+    # set the elasticsearch URL
+    echo "elasticsearch.url: \"$ELASTICSEARCH_URL\"" >> $KIBANA_CONF
+    echo "elasticsearch.username: kibana" >> $KIBANA_CONF
+    echo "elasticsearch.password: $USER_KIBANA4_SERVER_PWD" >> $KIBANA_CONF
+    echo "server.host:" $(hostname -I) >> $KIBANA_CONF
+
+    local ENCRYPTION_KEY=$(pwgen 64 1)
+    echo "xpack.security.encryptionKey: \"$ENCRYPTION_KEY\"" >> $KIBANA_CONF
+    echo "xpack.reporting.encryptionKey: \"$ENCRYPTION_KEY\"" >> $KIBANA_CONF
+    log "[configuration_and_plugins] x-pack security encryption key generated"
+
+    log "[configuration_and_plugins] installing xpack plugin"
+    sudo /usr/share/kibana/bin/kibana-plugin install x-pack
+    log "[configuration_and_plugins] installed xpack plugin"
+}
+
+old_install_service()
+{
+    log "[old_install_service] configuring makeshift service for kibana 4"
+    {
+      echo -e "# kibana"
+      echo -e "description \"Elasticsearch Kibana Service\""
+      echo -e ""
+      echo -e "start on starting"
+      echo -e "script"
+      echo -e "  /opt/kibana/bin/kibana"
+      echo -e "end script"
+      echo -e ""
+    } >> /etc/init/kibana.conf
+
+    chmod +x /etc/init/kibana.conf
+    sudo service kibana start
+}
+
+install_start_service()
+{
+    log "[install_start_service] configuring service for kibana to run at start"
+    sudo update-rc.d kibana defaults 95 10
+    log "[install_start_service] starting kibana!"
+    sudo service kibana start
+}
+
+old_install_sequence()
+{
+    log "[old_install_sequence] Starting the old install sequence for kibana"
+    sudo service kibana start
+    old_add_kibana_os_user
+    old_download_unzip_kibana
+    old_configuration_and_plugins
+    old_install_service
+    log "[old_install_sequence] Finished installation"
+}
+
+install_sequence()
+{
+    log "[install_sequence] Starting installation"
+    download_install_deb
+    install_pwgen
+    configuration_and_plugins
+    install_start_service
+    log "[install_sequence] Finished installation"
+}
+
+#########################
+# Installation sequence
+#########################
+
+if [ ${INSTALL_PLUGINS} -ne 0 ]; then
+  if dpkg --compare-versions "$KIBANA_VERSION" ">=" "5.0.0"; then
+    install_sequence
+  else
+    old_install_sequence
+  fi
 fi
-
-# Add upstart task and start kibana service
-cat << EOF > /etc/init/kibana.conf
-# kibana
-description "Elasticsearch Kibana Service"
-
-start on starting
-script
-    /opt/kibana/bin/kibana
-end script
-EOF
-
-chmod +x /etc/init/kibana.conf
-sudo service kibana start
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 PRETTY=$(printf '%dh:%dm:%ds\n' $(($ELAPSED_TIME/3600)) $(($ELAPSED_TIME%3600/60)) $(($ELAPSED_TIME%60)))
