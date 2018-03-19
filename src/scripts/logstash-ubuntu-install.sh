fd8d9e75e25f14376bfd7456d4b43cc2b8e665e6
Russ Cam <russ.cam@forloop.co.uk>
2018-03-19 17:59:23 +1000

    Start of add logstash to template
    Work in Progress

--- src/scripts/logstash-ubuntu-install.sh
+++ src/scripts/logstash-ubuntu-install.sh
@@ -5,6 +5,8 @@
 # Russ Cam (Elastic)
 #
 
+export DEBIAN_FRONTEND=noninteractive
+
 #########################
 # HELP
 #########################
@@ -13,6 +15,7 @@ help()
 {
     echo "This script installs logstash on a dedicated VM in the elasticsearch ARM template cluster"
     echo "Parameters:"
+    # TODO: Add parameters here
 
     echo "-h view this help content"
 }
@@ -28,7 +31,16 @@ log()
 log "Begin execution of Logstash script extension on ${HOSTNAME}"
 START_TIME=$SECONDS
 
-export DEBIAN_FRONTEND=noninteractive
+#########################
+# Preconditions
+#########################
+
+if [ "${UID}" -ne 0 ];
+then
+    log "Script executed without root permissions"
+    echo "You must be root to run this program." >&2
+    exit 3
+fi
 
 if service --status-all | grep -Fq 'logstash'; then
   log "Logstash already installed."
@@ -40,26 +52,20 @@ fi
 #########################
 
 #Script Parameters
-CLUSTER_NAME="elasticsearch"
 LOGSTASH_VERSION="6.2.1"
-ES_VERSION="6.2.1"
-#Default internal load balancer ip
 ELASTICSEARCH_URL="http://10.0.0.4:9200"
 INSTALL_XPACK=0
+INSTALL_ADDITIONAL_PLUGINS=""
 USER_LOGSTASH_PWD="changeme"
+LOGSTASH_KEYSTORE_PWD="changeme"
+LOGSTASH_CONF_FILE=""
 
 #Loop through options passed
-while getopts :n:v:e:u:S:C:K:P:m:lh optname; do
+while getopts :v:u:S:c:K:L:h optname; do
   log "Option $optname set"
   case $optname in
-    n) #set cluster name
-      CLUSTER_NAME="${OPTARG}"
-      ;;
     v) #logstash version number
       LOGSTASH_VERSION="${OPTARG}"
-      ;;
-    e) #elasticsearch version number
-      ES_VERSION="${OPTARG}"
       ;;
     u) #elasticsearch url
       ELASTICSEARCH_URL="${OPTARG}"
@@ -70,6 +76,14 @@ while getopts :n:v:e:u:S:C:K:P:m:lh optname; do
     l) #install X-Pack
       INSTALL_XPACK=1
       ;;
+    L) #install additional plugins
+      INSTALL_ADDITIONAL_PLUGINS="${OPTARG}"
+      ;;
+    c) #logstash configuration
+      LOGSTASH_CONF_FILE="${OPTARG}"
+      ;;
+    K) #logstash keystore password
+      LOGSTASH_KEYSTORE_PWD="${OPTARG}"
     h) #show help
       help
       exit 2
@@ -85,9 +99,8 @@ done
 # Parameter state changes
 #########################
 
-log "installing logstash $LOGSTASH_VERSION for Elasticsearch $ES_VERSION cluster: $CLUSTER_NAME"
+log "installing logstash $LOGSTASH_VERSION"
 log "installing X-Pack plugins is set to: $INSTALL_XPACK"
-log "Logstash will talk to Elasticsearch over $ELASTICSEARCH_URL"
 
 #########################
 # Installation steps as functions
@@ -95,84 +108,98 @@ log "Logstash will talk to Elasticsearch over $ELASTICSEARCH_URL"
 
 download_install_deb()
 {
-    log "[download_install_deb] starting download of package"
-    local DOWNLOAD_URL="https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-amd64.deb"
-    curl -o "kibana-$KIBANA_VERSION.deb" "$DOWNLOAD_URL"
-    log "[download_install_deb] installing downloaded package"
-    dpkg -i "kibana-$KIBANA_VERSION.deb"
+  log "[download_install_deb] starting download of package"
+  local DOWNLOAD_URL="https://artifacts.elastic.co/downloads/logstash/logstash-$LOGSTASH_VERSION.deb"
+  curl -o "logstash-$LOGSTASH_VERSION.deb" "$DOWNLOAD_URL"
+  log "[download_install_deb] installing downloaded package"
+  dpkg -i "logstash-$LOGSTASH_VERSION.deb"
 }
 
 ## Security
 ##----------------------------------
 
+add_keystore_or_env_var()
+{
+  if dpkg --compare-versions "$LOGSTASH_VERSION" ">=" "6.2.0"; then
+    log "[configuration_and_plugins] adding $1 to logstash keystore"
+    echo "$2" | bin/logstash-keystore add $1
+    log "[configuration_and_plugins] added $1 logstash keystore"
+  else
+    log "[add_keystore_or_env_var] adding environment variable for $1"
+    set +o history
+    export $1="$2"
+    set -o history
+    log "[add_keystore_or_env_var] added environment variable for $1"
+  fi
+}
+
+install_additional_plugins()
+{
+    SKIP_PLUGINS="x-pack"
+    log "[install_additional_plugins] installing additional plugins"
+    for PLUGIN in $(echo $INSTALL_ADDITIONAL_PLUGINS | tr ";" "\n")
+    do
+        if [[ $SKIP_PLUGINS =~ $PLUGIN ]]; then
+            log "[install_additional_plugins] skipping plugin $PLUGIN"
+        else
+            log "[install_additional_plugins] installing plugin $PLUGIN"
+            /usr/share/logstash/bin/logstash-plugin install $PLUGIN
+            log "[install_additional_plugins] installed plugin $PLUGIN"
+        fi
+    done
+    log "[install_additional_plugins] installed additional plugins"
+}
+
 configuration_and_plugins()
 {
     # backup the current config
-    mv /etc/kibana/kibana.yml /etc/kibana/kibana.yml.bak
+    local LOGSTASH_CONF=/etc/logstash/logstash.yml
+    mv "$LOGSTASH_CONF" "$LOGSTASH_CONF.bak"
 
-    log "[configuration_and_plugins] configuring kibana.yml"
-    local KIBANA_CONF=/etc/kibana/kibana.yml
-    # set the elasticsearch URL
-    echo "elasticsearch.url: \"$ELASTICSEARCH_URL\"" >> $KIBANA_CONF
-    echo "server.host:" $(hostname -I) >> $KIBANA_CONF
-    # specify kibana log location
-    echo "logging.dest: /var/log/kibana.log" >> $KIBANA_CONF
-    touch /var/log/kibana.log
-    chown kibana: /var/log/kibana.log
+    log "[configuration_and_plugins] configuring logstash.yml"
 
-    # set logging to silent by default
-    echo "logging.silent: true" >> $KIBANA_CONF
+    mkdir -p /var/log/logstash
+    chown -R logstash: /var/log/logstash
+
+    # logstash conf file
+    if [[ -n "$LOGSTASH_CONF_FILE" ]]; then
+      echo "$LOGSTASH_CONF_FILE" > /etc/logstash/conf.d/logstash.conf
+    fi
+
+    # logstash keystore
+    if dpkg --compare-versions "$LOGSTASH_VERSION" ">=" "6.2.0"; then
+      set +o history
+      export LOGSTASH_KEYSTORE_PASS="$LOGSTASH_KEYSTORE_PWD"
+      set -o history
+      log "[configuration_and_plugins] creating logstash keystore"
+      bin/logstash-keystore create
+      log "[configuration_and_plugins] created logstash keystore"
+    fi
+
+    add_keystore_or_env_var 'LOGSTASH_SYSTEM_PASS' "$USER_LOGSTASH_PWD"
+    add_keystore_or_env_var 'ELASTICSEARCH_URL' "$ELASTICSEARCH_URL"
 
     # install x-pack
     if [ ${INSTALL_XPACK} -ne 0 ]; then
-      echo "elasticsearch.username: kibana" >> $KIBANA_CONF
-      echo "elasticsearch.password: $USER_KIBANA_PWD" >> $KIBANA_CONF
-
-      install_pwgen
-      local ENCRYPTION_KEY=$(pwgen 64 1)
-      echo "xpack.security.encryptionKey: \"$ENCRYPTION_KEY\"" >> $KIBANA_CONF
-      ENCRYPTION_KEY=$(pwgen 64 1)
-      echo "xpack.reporting.encryptionKey: \"$ENCRYPTION_KEY\"" >> $KIBANA_CONF
-      log "[configuration_and_plugins] x-pack security encryption key generated"
+      echo "xpack.monitoring.elasticsearch.username: logstash_system" >> $LOGSTASH_CONF
+      echo 'xpack.monitoring.elasticsearch.password: ${LOGSTASH_SYSTEM_PASS}' >> $LOGSTASH_CONF
 
       log "[configuration_and_plugins] installing x-pack plugin"
-      /usr/share/kibana/bin/kibana-plugin install x-pack
+      /usr/share/logstash/bin/logstash-plugin install x-pack
       log "[configuration_and_plugins] installed x-pack plugin"
     fi
 
-    # configure HTTPS if cert and private key supplied
-    if [[ -n "${SSL_CERT}" && -n "${SSL_KEY}" ]]; then
-      mkdir -p /etc/kibana/ssl
-      log "[configuration_and_plugins] save kibana cert blob to file"
-      echo ${SSL_CERT} | base64 -d | tee /etc/kibana/ssl/kibana.crt
-      log "[configuration_and_plugins] save kibana key blob to file"
-      echo ${SSL_KEY} | base64 -d | tee /etc/kibana/ssl/kibana.key
-
-      log "[configuration_and_plugins] configuring encrypted communication"
-
-      if dpkg --compare-versions "$KIBANA_VERSION" ">=" "5.3.0"; then
-          echo "server.ssl.enabled: true" >> $KIBANA_CONF
-          echo "server.ssl.key: /etc/kibana/ssl/kibana.key" >> $KIBANA_CONF
-          echo "server.ssl.certificate: /etc/kibana/ssl/kibana.crt" >> $KIBANA_CONF
-
-          if [[ -n "${SSL_PASSPHRASE}" ]]; then
-              echo "server.ssl.keyPassphrase: \"$SSL_PASSPHRASE\"" >> $KIBANA_CONF
-          fi
-      else
-          echo "server.ssl.key: /etc/kibana/ssl/kibana.key" >> $KIBANA_CONF
-          echo "server.ssl.cert: /etc/kibana/ssl/kibana.crt" >> $KIBANA_CONF
-      fi
-
-      log "[configuration_and_plugins] configured encrypted communication"
+    # install additional plugins
+    if [[ -n "$INSTALL_ADDITIONAL_PLUGINS" ]]; then
+      install_additional_plugins
     fi
 }
 
 install_start_service()
 {
-    log "[install_start_service] configuring service for kibana to run at start"
-    update-rc.d kibana defaults 95 10
-    log "[install_start_service] starting kibana!"
-    service kibana start
+    log "[install_start_service] starting logstash"
+    systemctl start logstash.service
+    log "[install_start_service] started logstash!"
 }
 
 #########################
@@ -191,4 +218,4 @@ log "[install_sequence] Finished installation"
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 PRETTY=$(printf '%dh:%dm:%ds\n' $(($ELAPSED_TIME/3600)) $(($ELAPSED_TIME%3600/60)) $(($ELAPSED_TIME%60)))
-log "End execution of Kibana script extension in ${PRETTY}"
+log "End execution of Logstash script extension in ${PRETTY}"
