92532eb160c08081332138874c7a385bdf550eb4
Patrick Pun <panyang1217@hotmail.com>
2018-04-12 11:59:01 +0800

    add non-global azure environment supporting to testing scripts.

--- build/tasks/arm-validator.js
+++ build/tasks/arm-validator.js
@@ -70,7 +70,7 @@ var bootstrap = (cb) => {
   var versions = _.keys(allowedValues.versions);
   var defaultVersion = _.last(versions);
   git.branch(function (branch) {
-    artifactsBaseUrl = "https://raw.githubusercontent.com/elastic/azure-marketplace/"+ branch + "/src";
+    artifactsBaseUrl = "https://raw.githubusercontent.com/panyang1217/azure-marketplace/"+ branch + "/src";
     templateUri = artifactsBaseUrl + "/mainTemplate.json";
     log("Using template: " + templateUri, false);
     armTests = _(fs.readdirSync("arm-tests"))
@@ -85,8 +85,10 @@ var login = (cb) => bootstrap(() => {
   var login = [ 'login', '--service-principal',
     '--username', config.arm.clientId,
     '--password', config.arm.clientSecret,
-    '--tenant', config.arm.tenantId
+    '--tenant', config.arm.tenantId,
+    '-e', config.arm.environment
   ];
+
   log("logging into azure cli tooling")
   var child = execFile(azureCli, login, (error, stdout, stderr) => {
     if (error || stderr) return bailOutNoCleanUp(error || new Error(stderr));
