CollabIP/elasticsearch-arm-templates

Add support for HTTP and Transport layer encryption
This commit adds support for securing HTTP and Transport layers with PKCS#12 archive certificates. This allows
easier configuration for a cluster that has a commercial license.

src/scripts/data-node-install.sh
@@ -82,6 +82,11 @@ USER_KIBANA_PWD="changeme"
 BOOTSTRAP_PASSWORD="changeme"
 ANONYMOUS_ACCESS=0
 
+HTTP_CERT=""
+HTTP_CERT_PASSWORD=""
+TRANSPORT_CERT=""
+TRANSPORT_CERT_PASSWORD=""
+
 API_URL=""
 MARKETING_ID=""
 COMPANY_NAME=""
@@ -94,7 +99,7 @@ COUNTRY=""
 INSTALL_SWITCHES=""
 
 #Loop through options passed
-while getopts :n:m:v:A:R:K:S:Z:p:U:I:c:e:f:g:t:s:o:a:k:L:C:B:Xxyzldjh optname; do
+while getopts :n:m:v:A:R:K:S:Z:p:U:I:c:e:f:g:t:s:o:a:k:L:C:B:H:G:T:W:Xxyzldjh optname; do
   log "Option $optname set"
   case $optname in
     n) #set cluster name
@@ -138,6 +143,18 @@ while getopts :n:m:v:A:R:K:S:Z:p:U:I:c:e:f:g:t:s:o:a:k:L:C:B:Xxyzldjh optname; d
       ;;
     C) #additional yaml configuration
       YAML_CONFIGURATION="${OPTARG}"
+      ;;
+    H) #HTTP cert blob
+      HTTP_CERT="${OPTARG}"
+      ;;
+    G) #HTTP cert password
+      HTTP_CERT_PASSWORD="${OPTARG}"
+      ;;
+    T) #Transport cert blob
+      TRANSPORT_CERT="${OPTARG}"
+      ;;
+    W) #Transport cert password
+      TRANSPORT_CERT_PASSWORD="${OPTARG}"
       ;;
     a) #azure storage account for azure cloud plugin
       STORAGE_ACCOUNT="${OPTARG}"
@@ -220,7 +237,7 @@ if [ $ANONYMOUS_ACCESS -eq 1 ]; then
 fi
 
 # install elasticsearch
-bash elasticsearch-ubuntu-install.sh -n "$CLUSTER_NAME" -m $ES_HEAP -v "$ES_VERSION" -A "$USER_ADMIN_PWD" -R "$USER_READ_PWD" -K "$USER_KIBANA_PWD" -S "$USER_LOGSTASH_PWD" -B "$BOOTSTRAP_PASSWORD" -Z "$DATANODE_COUNT" -p "$NAMESPACE_PREFIX" -a "$STORAGE_ACCOUNT" -k "$STORAGE_KEY" -L "$INSTALL_ADDITIONAL_PLUGINS" -C "$YAML_CONFIGURATION" $INSTALL_SWITCHES
+bash elasticsearch-ubuntu-install.sh -n "$CLUSTER_NAME" -m $ES_HEAP -v "$ES_VERSION" -A "$USER_ADMIN_PWD" -R "$USER_READ_PWD" -K "$USER_KIBANA_PWD" -S "$USER_LOGSTASH_PWD" -B "$BOOTSTRAP_PASSWORD" -Z "$DATANODE_COUNT" -p "$NAMESPACE_PREFIX" -a "$STORAGE_ACCOUNT" -k "$STORAGE_KEY" -L "$INSTALL_ADDITIONAL_PLUGINS" -C "$YAML_CONFIGURATION" -H "$HTTP_CERT" -G "$HTTP_CERT_PASSWORD" -T "$TRANSPORT_CERT" -W "$TRANSPORT_CERT_PASSWORD" $INSTALL_SWITCHES
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   log "installing Elasticsearch returned exit code $EXIT_CODE"

elastic/azure-marketplace

src/scripts/data-node-install.sh
@@ -82,6 +82,11 @@ USER_KIBANA_PWD="changeme"
 BOOTSTRAP_PASSWORD="changeme"
 ANONYMOUS_ACCESS=0
 
+HTTP_CERT=""
+HTTP_CERT_PASSWORD=""
+TRANSPORT_CERT=""
+TRANSPORT_CERT_PASSWORD=""
+
 API_URL=""
 MARKETING_ID=""
 COMPANY_NAME=""
@@ -94,7 +99,7 @@ COUNTRY=""
 INSTALL_SWITCHES=""
 
 #Loop through options passed
-while getopts :n:m:v:A:R:K:S:Z:p:U:I:c:e:f:g:t:s:o:a:k:L:C:B:E:Xxyzldjh optname; do
+while getopts :n:m:v:A:R:K:S:Z:p:U:I:c:e:f:g:t:s:o:a:k:L:C:B:E:H:G:T:W:Xxyzldjh optname; do
   log "Option $optname set"
   case $optname in
     n) #set cluster name
@@ -138,6 +143,18 @@ while getopts :n:m:v:A:R:K:S:Z:p:U:I:c:e:f:g:t:s:o:a:k:L:C:B:E:Xxyzldjh optname;
       ;;
     C) #additional yaml configuration
       YAML_CONFIGURATION="${OPTARG}"
+      ;;
+    H) #HTTP cert blob
+      HTTP_CERT="${OPTARG}"
+      ;;
+    G) #HTTP cert password
+      HTTP_CERT_PASSWORD="${OPTARG}"
+      ;;
+    T) #Transport cert blob
+      TRANSPORT_CERT="${OPTARG}"
+      ;;
+    W) #Transport cert password
+      TRANSPORT_CERT_PASSWORD="${OPTARG}"
       ;;
     a) #azure storage account for azure cloud plugin
       STORAGE_ACCOUNT="${OPTARG}"
@@ -223,7 +240,7 @@ if [ $ANONYMOUS_ACCESS -eq 1 ]; then
 fi
 
 # install elasticsearch
-bash elasticsearch-ubuntu-install.sh -n "$CLUSTER_NAME" -m $ES_HEAP -v "$ES_VERSION" -A "$USER_ADMIN_PWD" -R "$USER_READ_PWD" -K "$USER_KIBANA_PWD" -S "$USER_LOGSTASH_PWD" -B "$BOOTSTRAP_PASSWORD" -Z "$DATANODE_COUNT" -p "$NAMESPACE_PREFIX" -a "$STORAGE_ACCOUNT" -k "$STORAGE_KEY" -E "$STORAGE_SUFFIX" -L "$INSTALL_ADDITIONAL_PLUGINS" -C "$YAML_CONFIGURATION" $INSTALL_SWITCHES
+bash elasticsearch-ubuntu-install.sh -n "$CLUSTER_NAME" -m $ES_HEAP -v "$ES_VERSION" -A "$USER_ADMIN_PWD" -R "$USER_READ_PWD" -K "$USER_KIBANA_PWD" -S "$USER_LOGSTASH_PWD" -B "$BOOTSTRAP_PASSWORD" -Z "$DATANODE_COUNT" -p "$NAMESPACE_PREFIX" -a "$STORAGE_ACCOUNT" -k "$STORAGE_KEY" -E "$STORAGE_SUFFIX" -L "$INSTALL_ADDITIONAL_PLUGINS" -C "$YAML_CONFIGURATION" -H "$HTTP_CERT" -G "$HTTP_CERT_PASSWORD" -T "$TRANSPORT_CERT" -W "$TRANSPORT_CERT_PASSWORD" $INSTALL_SWITCHES
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   log "installing Elasticsearch returned exit code $EXIT_CODE"