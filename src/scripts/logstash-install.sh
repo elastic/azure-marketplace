7b7bfbb4a23fc92564247c88f6b3056ffcb18402
Russ Cam <russ.cam@forloop.co.uk>
2018-09-14 16:44:20 +1000

    Add logstash linked template
    WIP

--- src/scripts/logstash-install.sh
+++ src/scripts/logstash-install.sh
@@ -42,33 +42,34 @@ then
     exit 3
 fi
 
-if service --status-all | grep -Fq 'logstash'; then
-  log "Logstash already installed."
-
-  # TODO: Ability to stop, change configuration and restart
-
-  exit 0
-fi
-
 #########################
 # Parameter handling
 #########################
 
 #Script Parameters
 LOGSTASH_VERSION="6.2.4"
+LOGSTASH_HEAP=0
 ELASTICSEARCH_URL="http://10.0.0.4:9200"
 INSTALL_XPACK=0
 INSTALL_ADDITIONAL_PLUGINS=""
 USER_LOGSTASH_PWD="changeme"
 LOGSTASH_KEYSTORE_PWD="changeme"
 LOGSTASH_CONF_FILE=""
+YAML_CONFIGURATION=""
+HTTP_CERT=""
+HTTP_CERT_PASSWORD=""
+HTTP_CACERT=""
+HTTP_CACERT_PASSWORD=""
 
 #Loop through options passed
-while getopts :v:u:S:c:K:L:h optname; do
+while getopts :v:m:u:S:H:G:V:J:L:c:K:Y:lh optname; do
   log "Option $optname set"
   case $optname in
     v) #logstash version number
       LOGSTASH_VERSION="${OPTARG}"
+      ;;
+    m) #heap_size
+      LOGSTASH_HEAP=${OPTARG}
       ;;
     u) #elasticsearch url
       ELASTICSEARCH_URL="${OPTARG}"
@@ -79,14 +80,30 @@ while getopts :v:u:S:c:K:L:h optname; do
     l) #install X-Pack
       INSTALL_XPACK=1
       ;;
+    H) #Elasticsearch certificate
+      HTTP_CERT="${OPTARG}"
+      ;;
+    G) #Elasticsearch certificate password
+      HTTP_CERT_PASSWORD="${OPTARG}"
+      ;;
+    V) #Elasticsearch CA certificate
+      HTTP_CACERT="${OPTARG}"
+      ;;
+    J) #Elasticsearch CA certificate password
+      HTTP_CACERT_PASSWORD="${OPTARG}"
+      ;;
     L) #install additional plugins
       INSTALL_ADDITIONAL_PLUGINS="${OPTARG}"
       ;;
-    c) #logstash configuration
+    c) #logstash configuration file
       LOGSTASH_CONF_FILE="${OPTARG}"
       ;;
     K) #logstash keystore password
       LOGSTASH_KEYSTORE_PWD="${OPTARG}"
+      ;;
+    Y) #logstash additional yml configuration
+      YAML_CONFIGURATION="${OPTARG}"
+      ;;
     h) #show help
       help
       exit 2
@@ -98,13 +115,6 @@ while getopts :v:u:S:c:K:L:h optname; do
       ;;
   esac
 done
-
-#########################
-# Parameter state changes
-#########################
-
-log "installing logstash $LOGSTASH_VERSION"
-log "installing X-Pack plugins is set to: $INSTALL_XPACK"
 
 #########################
 # Installation steps as functions
@@ -149,16 +159,16 @@ add_keystore_or_env_var()
     export LOGSTASH_KEYSTORE_PASS="$LOGSTASH_KEYSTORE_PWD"
     set -o history
 
-    # create it the keystore if it doesn't exist
+    # create keystore if it doesn't exist
     if [[ ! -f /etc/logstash/logstash.keystore ]]; then
-      log "[configure_logstash] creating logstash keystore"
+      log "[add_keystore_or_env_var] creating logstash keystore"
       /usr/share/logstash/bin/logstash-keystore create
-      log "[configure_logstash] created logstash keystore"
+      log "[add_keystore_or_env_var] created logstash keystore"
     fi
 
-    log "[configuration_and_plugins] adding $KEY to logstash keystore"
+    log "[add_keystore_or_env_var] adding $KEY to logstash keystore"
     echo "$VALUE" | /usr/share/logstash/bin/logstash-keystore add $KEY
-    log "[configuration_and_plugins] added $KEY logstash keystore"
+    log "[add_keystore_or_env_var] added $KEY logstash keystore"
   else
     log "[add_keystore_or_env_var] adding environment variable for $KEY"
     set +o history
@@ -168,13 +178,15 @@ add_keystore_or_env_var()
   fi
 }
 
-configure_logstash()
+configure_logstash_yaml()
 {
-    # backup the current config
     local LOGSTASH_CONF=/etc/logstash/logstash.yml
+    local SSL_PATH=/etc/logstash/ssl
+
+    # backup the current config
     mv $LOGSTASH_CONF $LOGSTASH_CONF.bak
 
-    log "[configure_logstash] configuring logstash.yml"
+    log "[configure_logstash_yaml] configuring logstash.yml"
 
     echo "node.name: \"${HOSTNAME}\"" >> $LOGSTASH_CONF
 
@@ -182,11 +194,12 @@ configure_logstash()
     if [[ -n "$LOGSTASH_CONF_FILE" ]]; then
       local CONF_FILE=/etc/logstash/conf.d/logstash.conf
       mv $CONF_FILE $CONF_FILE.bak
+      log "[configure_logstash_yaml] writing logstash conf to $CONF_FILE"
       echo ${LOGSTASH_CONF_FILE} | base64 -d | tee $CONF_FILE
     fi
 
     # allow values to be referenced in *.conf files
-    add_keystore_or_env_var 'LOGSTASH_SYSTEM_PASS' "$USER_LOGSTASH_PWD"
+    add_keystore_or_env_var 'LOGSTASH_SYSTEM_PASSWORD' "$USER_LOGSTASH_PWD"
     add_keystore_or_env_var 'ELASTICSEARCH_URL' "$ELASTICSEARCH_URL"
 
     # put data on the OS disk in a writable location
@@ -205,16 +218,62 @@ configure_logstash()
     # install x-pack
     if [ ${INSTALL_XPACK} -ne 0 ]; then
       if dpkg --compare-versions "$LOGSTASH_VERSION" "<" "6.3.0"; then
-        log "[configure_logstash] installing x-pack plugin"
+        log "[configure_logstash_yaml] installing x-pack plugin"
         /usr/share/logstash/bin/logstash-plugin install x-pack
-        log "[configure_logstash] installed x-pack plugin"
+        log "[configure_logstash_yaml] installed x-pack plugin"
       fi
 
+      # configure monitoring
+      echo 'xpack.monitoring.elasticsearch.url: "${ELASTICSEARCH_URL}"' >> $LOGSTASH_CONF
       echo "xpack.monitoring.elasticsearch.username: logstash_system" >> $LOGSTASH_CONF
-      # reference from env var or keystore
-      echo 'xpack.monitoring.elasticsearch.password: "${LOGSTASH_SYSTEM_PASS}"' >> $LOGSTASH_CONF
+      echo 'xpack.monitoring.elasticsearch.password: "${LOGSTASH_SYSTEM_PASSWORD}"' >> $LOGSTASH_CONF
       echo "xpack.monitoring.enabled: true" >> $LOGSTASH_CONF
     fi
+
+    # Make the HTTP CA cert for communication with Elasticsearch available to
+    # Logstash conf files through ${ELASTICSEARCH_CACERT}
+    if [[ -n "${HTTP_CERT}" || -n "${HTTP_CACERT}" && ${INSTALL_XPACK} -ne 0 ]]; then
+      [ -d $SSL_PATH ] || mkdir -p $SSL_PATH
+
+      if [[ -n "${HTTP_CERT}" ]]; then
+        # convert PKCS#12 certificate to PEM format
+        log "[configure_logstash_yaml] Save PKCS#12 archive for Elasticsearch HTTP to file"
+        echo ${HTTP_CERT} | base64 -d | tee $SSL_PATH/elasticsearch-http.p12
+        log "[configure_logstash_yaml] Extract CA cert from PKCS#12 archive for Elasticsearch HTTP"
+        echo "$HTTP_CERT_PASSWORD" | openssl pkcs12 -in $SSL_PATH/elasticsearch-http.p12 -out $SSL_PATH/elasticsearch-http-ca.crt -cacerts -nokeys -chain -passin stdin
+
+        log "[configure_logstash_yaml] Configuring ELASTICSEARCH_CACERT for Elasticsearch TLS"
+        if [[ $(stat -c %s $SSL_PATH/elasticsearch-http-ca.crt 2>/dev/null) -eq 0 ]]; then
+            log "[configure_logstash_yaml] No CA cert extracted from HTTP cert. Cannot make ELASTICSEARCH_CACERT available to conf files"
+        else
+            log "[configure_logstash_yaml] CA cert extracted from HTTP PKCS#12 archive. Make ELASTICSEARCH_CACERT available to conf files"
+            add_keystore_or_env_var "ELASTICSEARCH_CACERT" "$SSL_PATH/elasticsearch-http-ca.crt"
+            echo 'xpack.monitoring.elasticsearch.ssl.ca: "${ELASTICSEARCH_CACERT}"' >> $LOGSTASH_CONF
+        fi
+
+      else
+
+        # convert PKCS#12 CA certificate to PEM format
+        local HTTP_CACERT_FILENAME=elasticsearch-http-ca.p12
+        log "[configure_logstash_yaml] Save PKCS#12 archive for Elasticsearch HTTP CA to file"
+        echo ${HTTP_CACERT} | base64 -d | tee $SSL_PATH/$HTTP_CACERT_FILENAME
+        log "[configure_logstash_yaml] Convert PKCS#12 archive for Elasticsearch HTTP CA to PEM format"
+        echo "$HTTP_CACERT_PASSWORD" | openssl pkcs12 -in $SSL_PATH/$HTTP_CACERT_FILENAME -out $SSL_PATH/elasticsearch-http-ca.crt -clcerts -nokeys -chain -passin stdin
+
+        log "[configure_logstash_yaml] Configuring ELASTICSEARCH_CACERT for Elasticsearch TLS"
+        if [[ $(stat -c %s $SSL_PATH/elasticsearch-http-ca.crt 2>/dev/null) -eq 0 ]]; then
+            log "[configure_logstash_yaml] No CA cert extracted from HTTP CA PKCS#12 archive. Cannot make ELASTICSEARCH_CACERT available to conf files"
+        else
+            log "[configure_logstash_yaml] CA cert extracted from HTTP CA PKCS#12 archive. Make ELASTICSEARCH_CACERT available to conf files"
+            add_keystore_or_env_var "ELASTICSEARCH_CACERT" "$SSL_PATH/elasticsearch-http-ca.crt"
+            echo 'xpack.monitoring.elasticsearch.ssl.ca: "${ELASTICSEARCH_CACERT}"' >> $LOGSTASH_CONF
+        fi
+      fi
+      chown -R logstash: $SSL_PATH
+      log "[configure_logstash_yaml] Configured ELASTICSEARCH_CACERT for Elasticsearch TLS"
+    fi
+
+    # TODO: Configure Pipeline Management?
 
     # Additional yaml configuration
     if [[ -n "$YAML_CONFIGURATION" ]]; then
@@ -246,6 +305,15 @@ configure_logstash()
             log "[configure_logstash] errors in yaml configuration. exiting"
             exit 11
         fi
+    fi
+}
+
+configure_logstash()
+{
+    if [[ "$LOGSTASH_HEAP" -ne "0" ]]; then
+      log "[configure_logstash] configure logstash heap size - $LOGSTASH_HEAP"
+      sed -i -e "s/^\-Xmx.*/-Xmx${LOGSTASH_HEAP}m/" /etc/logstash/jvm.options
+      sed -i -e "s/^\-Xms.*/-Xms${LOGSTASH_HEAP}m/" /etc/logstash/jvm.options
     fi
 }
 
@@ -292,6 +360,20 @@ install_yamllint()
 # Installation sequence
 #########################
 
+if systemctl -q is-enabled logstash.service; then
+  log "logstash already installed."
+
+  configure_logstash_yaml
+
+  # restart logstash if config has changed
+  cmp --silent /etc/logstash/logstash.yml /etc/logstash/logstash.yml.bak \
+    || systemctl reload-or-restart logstash.service
+
+  exit 0
+fi
+
+log "installing logstash $LOGSTASH_VERSION"
+log "installing X-Pack plugins is set to: $INSTALL_XPACK"
 log "[apt-get] updating apt-get"
 (apt-get -y update || (sleep 15; apt-get -y update)) > /dev/null
 log "[apt-get] updated apt-get"
@@ -300,7 +382,9 @@ install_java
 
 install_logstash
 
-configuration_and_plugins
+configure_logstash_yaml
+
+configure_logstash
 
 # install additional plugins
 if [[ -n "$INSTALL_ADDITIONAL_PLUGINS" ]]; then
