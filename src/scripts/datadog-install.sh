b8141425e1336c37c91a220286b19cef8dd2dca7
Matt Hintzke <matt.hintzke@skykick.com>
2018-10-29 11:41:11 -0700

    Using variables here instead of repeatin

--- src/scripts/datadog-install.sh
+++ src/scripts/datadog-install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+ELASTIC_CONFIG_FILE=/etc/datadog-agent/conf.d/elastic.d/elastic.yaml
+DD_AGENT=/var/tmp/install_datadog_agent.sh
+
 # Custom logging with time so we can easily relate running times, also log to separate file so order is guaranteed.
 # The Script extension output the stdout/err buffer in intervals with duplicates.
 log()
@@ -16,19 +19,19 @@ fi
 
 log "Installing DataDog plugin onto this machine using API Key [$1]"
 
-wget -q https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh -O /var/tmp/install_datadog_agent.sh
-chmod +x /var/tmp/install_datadog_agent.sh
-DD_API_KEY=$1 /var/tmp/install_datadog_agent.sh
+wget -q https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh -O $DD_AGENT
+chmod +x $DD_AGENT
+DD_API_KEY=$1 $DD_AGENT
 rm /etc/datadog-agent/conf.d/elastic.d/auto_conf.yaml
-echo "ad_identifiers:" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "  - elasticsearch" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "instances:" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "  - url: 'http://localhost:9200'" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "    pshard_stats: true" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "    cluster_stats: false" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "    pending_task_stats: true" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "    tags:" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
-echo "      - 'elasticsearch-role:data-node'" | tee -a /etc/datadog-agent/conf.d/elastic.d/elastic.yaml
+echo "ad_identifiers:" | tee -a $ELASTIC_CONFIG_FILE
+echo "  - elasticsearch" | tee -a $ELASTIC_CONFIG_FILE
+echo "instances:" | tee -a $ELASTIC_CONFIG_FILE
+echo "  - url: 'http://localhost:9200'" | tee -a $ELASTIC_CONFIG_FILE
+echo "    pshard_stats: true" | tee -a $ELASTIC_CONFIG_FILE
+echo "    cluster_stats: false" | tee -a $ELASTIC_CONFIG_FILE
+echo "    pending_task_stats: true" | tee -a $ELASTIC_CONFIG_FILE
+echo "    tags:" | tee -a $ELASTIC_CONFIG_FILE
+echo "      - 'elasticsearch-role:data-node'" | tee -a $ELASTIC_CONFIG_FILE
 systemctl restart datadog-agent
 
 log "Finished installing DataDog plugin..."
